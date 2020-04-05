a65fe56eda7f3e108a3c63a90cd8d2b6841502ad
Alberto017 <xavihdz09@gmail.com>
2020-04-04 18:44:41 -0700

    feat: Creation of project structure

---
+++ app/src/main/java/com/example/sanchez/itinerarios/ItinerarioActivity.java
@@ -0,0 +1,13 @@
+package com.example.sanchez.itinerarios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItinerarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itinerario);
+    }
+}
