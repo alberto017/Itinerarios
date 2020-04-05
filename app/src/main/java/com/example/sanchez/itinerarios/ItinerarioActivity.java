0bb9be7b940d264d5b292838404c928dfb56f719
Alberto017 <xavihdz09@gmail.com>
2020-04-05 16:47:09 -0700

    feat: Create SQLite connection

--- app/src/main/java/com/example/sanchez/itinerarios/ItinerarioActivity.java
+++ app/src/main/java/com/example/sanchez/itinerarios/ItinerarioActivity.java
@@ -1,13 +1,31 @@
 package com.example.sanchez.itinerarios;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.example.sanchez.itinerarios.model.ConexionSQLiteHelper;
 
 public class ItinerarioActivity extends AppCompatActivity {
+
+    private FrameLayout flItinerarioActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_itinerario);
+        flItinerarioActivity = findViewById(R.id.flItinerarioActivity);
+
+        ConexionSQLiteHelper conexionSQLiteHelper = new ConexionSQLiteHelper(this,"bd_itinerario",null,1);
+
+        setFragment(new ListItinerarioFragment());
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(flItinerarioActivity.getId(),fragment);
+        fragmentTransaction.commit();
     }
 }
